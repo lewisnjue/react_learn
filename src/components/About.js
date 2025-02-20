c725bdfb904fe863643b52a222928f5aaae0d7a8
lewis njuw <lewiskinyuanjue.ke@gmail.com>
2025-02-20 17:58:02 +0000

    removed old staff

--- src/components/About.js
+++
@@ -1,7 +0,0 @@
-function About(){
-    return (
-        <h1>this is about page</h1>
-    );
-}
-
-export default About;
--- src/components/Video.js
+++
@@ -1,8 +0,0 @@
-function Video(){
-    return (
-        <div>
-            <h1>React Player Example</h1>
-        </div>
-    );
-}
-export default Video;
